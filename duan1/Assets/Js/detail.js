02bce45e82e0fa4bd8a57c41ebc4d8e6fc85089f
Do Bao <baoanh25122004@gmail.com>
2026-04-17 14:57:46 +0700

    chỉnh sửa

--- duan1/Assets/Js/detail.js
+++
@@ -1,9 +0,0 @@
-function selectSize(element, size) {
-    var sizes = document.querySelectorAll('.size-options .size');
-    sizes.forEach(function(size) {
-        size.classList.remove('selected');
-    });
-
-    element.classList.add('selected');
-    document.getElementById('selected_size').value = size;
-}
--- duan1/app/Views/Admin/base/main.php
+++
@@ -1,6 +0,0 @@
-<?php include_once VIEW . 'Admin/base/header.php'; ?>
-<div class="content">
-    <h1>Trang quản trị</h1>
-    <!-- Nội dung động sẽ hiển thị ở đây -->
-</div>
-<?php include_once VIEW . 'Admin/base/footer.php'; ?>
